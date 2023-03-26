8d722c9b9a26508befdd4be6e85d24c03fd7a896
YuraJian <ur.duimja@gmail.com>
2023-03-26 13:52:20 +0300

    first commit

---
+++ hw1/src/App.js
@@ -0,0 +1,84 @@
+import React from "react";
+import "./App.css";
+
+const HomeComponent = () => {
+  return <div>Home Page</div>;
+};
+
+const NewsComponent = () => {
+  return <div>News Page</div>;
+};
+
+const CallBackComponent = () => {
+  return <div>CallBack Page</div>;
+};
+
+const ContactsComponent = () => {
+  return <div>Contacts Page</div>;
+};
+
+const links = [
+  {
+    id: "1",
+    title: "Home",
+    name: "home",
+    href: "#",
+  },
+  {
+    id: "2",
+    title: "News",
+    name: "news",
+    href: "#",
+  },
+  {
+    id: "3",
+    title: "CallBack",
+    name: "callback",
+    href: "#",
+  },
+  {
+    id: "4",
+    title: "Contacts",
+    name: "contacts",
+    href: "#",
+  },
+];
+
+const pages = {
+  home: <HomeComponent />,
+  news: <NewsComponent />,
+  callback: <CallBackComponent />,
+  contacts: <ContactsComponent />,
+};
+
+class NavComponent extends React.Component {
+  state = { currentPage: "home" };
+  
+  handleComponent = (name) => {
+    this.setState({ currentPage: name });
+  };
+
+  render() {
+    return (
+      <div>
+        <nav>
+          {links.map(({ id, title, name, href }) => (
+            <a
+            className="header-link"
+              name={name}
+              href={href}
+              key={id}
+              onClick={() => this.handleComponent(name)}
+            >
+              {title}
+            </a>
+          ))}
+        </nav>
+        <div>{pages[this.state.currentPage]}</div>
+      </div>
+    );
+  }
+
+}
+
+export default NavComponent;
